91d1971310dc912768288c7cd5518976afe49cea
L34310 <L34310@lab.aca>
2023-01-30 19:50:22 -0500

    Se guardo cambios

--- src/Presentation/Main/MainPage.jsx
+++ src/Presentation/Main/MainPage.jsx
@@ -8,19 +8,41 @@ import ListaPeliculas from "./components/ListaPeliculas"
     debo registrarme para entrar.
     */
 }
-function MainPage() {
-    const listaPeliculas = [
-        {
-            id : 1,
-            nombre : "Avatar 2",
-            url: "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg"
-        },
-        {
-            id : 2,
-            nombre : "Morbius",
-            url: "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/morbius_onesheet_1400x2100_he.jpg?itok=-jQVkWIe"
-        }
-    ]
+function MainPage(){ 
+//Comentar todo, ctr+shift(flecha arriba)+p : luego pone comentario
+     /*const listPeli = [
+         {
+             id : 1,
+             nombre : "Avatar 2",
+             url: "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg"
+         },
+         {
+             id : 2,
+             nombre : "Jujutsu Kaisen",
+             url: "https://es.web.img3.acsta.net/pictures/22/05/18/16/23/5993750.jpg"
+         },
+         {
+             id : 3,
+             nombre : "Cars",
+             url: "https://lumiere-a.akamaihd.net/v1/images/og_cars_lightningmcqueenday_18244_4435f27a.jpeg?region=40,0,1120,630"
+         }
+     ]*/
+    //Con el state podemos enviar un objeto javaScript y se guarda esta info
+    //en la pagina (LoginPage), sino
+    const obtenerPeliculas = function(){
+        //Abre un canal de comunicacion con el servidor y realiza una peticion http
+        //Ademas no te devuelve una lista de pelicula sino una promesa.
+        //Una promesa de ejecucion, vamos a definir que se va ejecutar cuando termine su peticion
+        //http
+        fetch("https://script.google.com/a/macros/ulima.edu.pe/s/AKfycbzRqLpRf7PXLuNQrgTKSTer6-Zt0dfmPmdDh-WmEr_dEm34Eh4qsfhMOADDoWgNKzdd/exec?entity=peliculas")
+        //La funcion then te permite ejecutar el codigo cuando termine la peticion fetch.
+        //then es una funcion asincrona, asique el console puede pintar antes o dsps.
+        promesa.then(function (response){
+            console.log("respuesta de servidor")
+        })
+        console.log("fin de la funcion obtener peliculas")
+    }
+    obtenerPeliculas()
     const location = useLocation()
     const navigate = useNavigate()
     console.log(location)
@@ -29,14 +51,12 @@ function MainPage() {
         hub
         Efecto secundario (ingles), es el efecto secundario de la primera renderizacion de tu
         codigo en otra palabras... 
-        lo que este dentro de la funcion useEffect se ejecutara en luego de la primera 
-        renderizacion. Es decir, en la 2da, 3ra, etc veces q se ejecute el cod
-        Luego de la segunda renderizacion, las demas lineas de codigo actuaran segun
-        a lo que responda el useEffect.
-        Si no se aplica esto, la lineas de cod que se necesita ejecutar una vez producira
-        errores. 
-        Necesitamos de esta funcion para que primero ejecute el div vacio "<div></div> "
-        y luego lo enviar rapidamente al "/", es casi imperseptible
+        lo que este dentro de la funcion useEffect se ejecutara la primera vez que se renderice.
+        Renderizar es pintar de manera grafica la pagina web pero en react simplemente es ejecutar
+        el codigo.
+        Necesitamos de esta funcion para evitar un esfuerzo computacional, sino lo pusieramos 
+        nos sale un error en donde el mismo react nos recomienda utilizar esta funcion para
+        utilizarlo.
         useEffect(function(){
         }, [])
         dentro de []= podemos hacer que el cod se renderice una vez mas cuando cambien algunas
@@ -54,22 +74,24 @@ function MainPage() {
         navigate("/")
         }
     },[])
-{
+
     /* Operador Ternario:
         Es una "Expresion" = bloque de codigo que va a devolver un valor.
         <condicion>?<si_es_true>:<si_es_false>
         return location.state !== null ? <div>Main Page: {location.state.username}</div>:<div></div>
         A diferencia de los "if", no de vuelve ningun valor.
     */
-}
+
     //con location.state podemos acceder al objeto que se envia desde la pag LoginPage
     //Aca el div vacio no importa porque rapidamente lo enviara a "/", pero si pasa por aqui
     //aunque es casi imperseptible. 
     
     //En vez de esos <div> en true deberia ir lo siguiente
     //<ListaPeliculas peliculas = {ListaPeliculas}/>
+    //O podemos usar esto
+    //Main Page: {location.state.username}
      return location.state !== null?
-     <div>Main Page: {location.state.username}</div> : <div></div> 
+     <div><ListaPeliculas peliculas = {[]}/></div> : <div></div> 
 
 }
 
--- src/Presentation/Main/components/ListaPeliculas.jsx
+++ src/Presentation/Main/components/ListaPeliculas.jsx
@@ -1,5 +1,91 @@
-function ListaPeliculas(props){
-    
+import CardPelicula from "./CardPelicula"
 
+function ListaPeliculas(props){
+    {
+        /*
+                                             El codigo:
+        return <div className="row">
+        {
+        props.peliculas.map(function(pelicula){
+            <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+        })
+    }
+    </div>
+                                              Lo interpretaria asi:
+    <div className="row">
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+    </div>
+        El proble con esto es que el md-6 suma mas de 12. Yo deseo que luego de 2, creee otro div:
+      
+    <div className="row">
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+    </div>
+
+    <div className="row">
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+        <div className="col-md-6">
+            <CardPelicula/>
+        </div>
+    </div>
+      
+        */
+    }
+    const listarow = []//Lista de filas
+    let listacol = []//Lista de columnas
+    //la funcion map ejecuta la funcion en cada elemento de la lista de manera PARALELA
+    //En for seria secuencial. PERO el forEach() funciona de manera a map pero se ejecuta de forma //
+    //SECUENCIAL
+    props.peliculas.forEach(function(pelicula, index){
+        if(index % 2 === 0){//indices pares, para que cada que 2 "col" se cree un "row" 
+            //lo que te permite el ` es convertirle en un String. AltGr y teclado al costado de enter
+            
+            listacol.push(
+                <div className="col-md-6"><CardPelicula peli={pelicula}/></div>
+            )
+        }else{
+            listacol.push(
+                <div className="col-md-6"><CardPelicula peli={pelicula}/></div>
+            )
+            listarow.push(
+                <div className="row">{ listacol }</div>
+            )
+            listacol = []
+        }
+    })
+    //Como pinta de 2 e 2, Si el numero de peliculas es impar (faltara una pelicula por pintar)
+    //Es por esto que ponemos:
+    if(props.peliculas.length % 2 !== 0){
+        listacol = []
+        listacol.push(
+            <div className="col-md-6"><CardPelicula peli={props.peliculas[props.peliculas.length-1]} />
+            </div>
+        )
+        listarow.push(
+            <div className="row">{ listacol }</div>
+        )
+    }
+
+    return <div>
+        {
+            listarow//Retornar lista filas
+        }
+    </div>
 }
 export default ListaPeliculas
